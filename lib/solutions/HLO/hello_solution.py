2ba736f8f48915cf3cf025038b3e22e27be2513b
sbx_user1051 <sbx_user1051@169.254.95.145>
2025-05-02 14:27:36 +0000

    Fri May 02 14:27:36 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,6 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name:str) -> str:
-        return "Hello, World!"
+        if not isinstance(friend_name, str):
+            raise TypeError("friend_name must be a string")
+        return f"Hello, {friend_name}!"
