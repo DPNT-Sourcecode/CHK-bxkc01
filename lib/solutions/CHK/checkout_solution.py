6ad8805f81387a4e8ea49783769ae76c76b00e75
sbx_user1051 <sbx_user1051@169.254.95.145>
2025-05-02 14:41:02 +0000

    Fri May 02 14:41:02 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,27 @@
+from collections import Counter
 
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        count = Counter(skus)
+        total = 0
+
+        if any(not sku.isalpha() and not sku.isupper() for sku in skus):
+            return -1
+
+        while count['A'] >= 3:
+            count['A'] -= 3
+            total += 130
+
+        while count['B'] >= 2:
+            count['B'] -= 2
+            total += 45
+            
+        total += count['A'] * 50 
+        total += count['B'] * 30
+        total += count['C'] * 20
+        total += count['D'] * 15
+        
+        return total
+
