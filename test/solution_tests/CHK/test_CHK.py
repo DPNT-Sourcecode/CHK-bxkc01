63fd6939d0ffab8d2fa2dd7a8c710d510f6375cb
sbx_user1051 <sbx_user1051@169.254.95.145>
2025-05-02 15:11:11 +0000

    Fri May 02 15:11:11 UTC 2025

--- test/solution_tests/CHK/test_CHK.py
+++
@@ -1,8 +0,0 @@
-from lib.solutions import checkout_solution
-
-class Test_CHK:
-    def test_CHK(self):
-        assert checkout_solution.CheckoutSolution().checkout("A") == 50
-        assert checkout_solution.CheckoutSolution().checkout("ABCD") == 115
-        assert checkout_solution.CheckoutSolution().checkout("AAAAA") == 175
-        
