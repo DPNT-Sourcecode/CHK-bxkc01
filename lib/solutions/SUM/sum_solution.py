4b5f1dc1c6544cb8187a87ea65bba20a57c94063
sbx_user1051 <sbx_user1051@169.254.95.145>
2025-05-02 14:21:22 +0000

    Fri May 02 14:21:22 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,9 @@
 
 class SumSolution:
     
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def compute(self, a:int, b:int) -> int:
+        if not (0 <= a <=100 and 0 <= b <=100):
+            raise ValueError('Numbers not in range')
+    
+        return a+b
+    
